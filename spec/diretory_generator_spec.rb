4e9d0922697233b558572b88993ddfabbb82f879
harryuan65 <harryuan.65@gmail.com>
2023-03-30 17:49:00 +0800

    feat: version 1

--- spec/diretory_generator_spec.rb
+++ spec/diretory_generator_spec.rb
@@ -1,11 +1,53 @@
 # frozen_string_literal: true
 
+require "psych"
+TEST_CASE_PATH = "spec/fixture/files/simple.yml"
+#                   - Basics                      - Root File
+#               /                \
+#             Variables           Some File
+#             /    |   \
+#         Decla.. Scope Type...
+#                 / | \
+#         Global Local Const..
+#
+
+EXPECTED = [{ "./Basics/Variables/" => "Declaration and Initialization" },
+            { "./Basics/Variables/Scope/" => "Global" },
+            { "./Basics/Variables/Scope/" => "Local" },
+            { "./Basics/Variables/Scope/" => "Constant and Immutable" },
+            { "./Basics/Variables/" => "Type Inference" },
+            { "./Basics/" => "Some File" },
+            { "./" => "Root File" }].freeze
+
 RSpec.describe DirectoryGenerator do
   it "has a version number" do
     expect(DirectoryGenerator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  subject(:given_valid_yaml) { DirectoryGenerator::Runner.new(TEST_CASE_PATH) }
+  context "when dry running(default)" do
+    it "generates a list of expected directory entries" do
+      # Expand ./Basics... to actual paths
+      expected_list_expanded = EXPECTED.map do |pair|
+        path, content = pair.entries.first
+        { File.expand_path(path) => content }
+      end
+
+      expect(given_valid_yaml.generate_directories).to eq(expected_list_expanded)
+    end
+  end
+
+  context "when performing" do
+    let(:example_target_root) { "testing123" }
+    it "generates expected directories" do
+      given_valid_yaml.generate_directories(root_path: example_target_root, dry_run: false)
+
+      EXPECTED.each do |pair|
+        path, content = pair.entries.first
+        path_to_file = File.expand_path(File.join(example_target_root, path, content))
+        expect(File.file?(path_to_file)).to eq(true), "expected #{path_to_file} to be a file"
+      end
+    end
+    after { FileUtils.rm_rf(File.expand_path(example_target_root)) }
   end
 end
